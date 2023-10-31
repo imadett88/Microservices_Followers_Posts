3c8fa45b1cd25f1aab526a582924b17a0ecd4777
imadettamen <imadettamen18@gmail.com>
2023-10-31 09:57:24 +0100

    COV2

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloGatewayDriver,
+  ApolloGatewayDriverConfig,
+} from '@nestjs/apollo/';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'followers',
+              url: 'http://localhost:3000/graphql',
+            },
+            {
+              name: 'posts',
+              url: 'http://localhost:7000/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
